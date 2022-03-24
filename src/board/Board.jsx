a0ef0382d03876520c60435182e12ef1faf3e344
Valerii Bihun <valeriibihun.co@gmail.com>
2022-03-23 20:38:22 -0600

    File structure created

---
+++ src/board/Board.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Ticket from '../ticket/Ticket'
+
+const Board = () => {
+  return (
+    <div>Board</div>
+  )
+}
+
+export default Board
---
+++ src/ticket/Ticket.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Ball from '../ball/Ball'
+
+const Ticket = () => {
+  return (
+    <div>Ticket</div>
+  )
+}
+
+export default Ticket
